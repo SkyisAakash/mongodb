d39f86a5c8302ba777f95301e5b06f9767cab633
Aakash Sarang <sarangaakash@gmail.com>
2018-08-19 13:41:27 -0700

    Write and pass tests for reading saved users

---
+++ users/test/reading_test.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const User = require('../src/user');
+describe('Reading user out of database', () => {
+    let joe;
+    beforeEach((done) => {
+        joe = new User({name: 'Joe'});
+        joe.save()
+        .then(() => done());
+    })
+    it('Finds all the users with name joe', (done) => {
+        User.find({ name: 'Joe' })
+            .then((users) => {
+                // console.log(users);
+                assert(users[0]._id.toString() === joe._id.toString());
+                done();
+            });
+    });
+});
