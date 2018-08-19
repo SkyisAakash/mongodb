dc629feaa86b329c0cf305490306f49cb7c478a8
Aakash Sarang <sarangaakash@gmail.com>
2018-08-19 13:25:03 -0700

    Modify code to connect mongoose to database only once before beginning of tests

--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+//before is executed only one time before test
+before((done) => {
+    mongoose.connect('mongodb://localhost/users_test');
 
-mongoose.connect('mongodb://localhost/users_test');
+    mongoose.connection
+        .once('open', () => { done(); })
+        .on('error', (error) => {
+            console.warn('warning', error);
+        });
+});
 
-mongoose.connection
-    .once('open', () => console.log('Good to go!'))
-    .on('error', (error) => {
-        console.warn('warning', error);
-    });
 
     beforeEach((done) => {
         mongoose.connection.collections.users.drop(() => {
