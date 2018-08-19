7bf95031a59e506a955ce06c881d64f7cc3ebe6b
Aakash Sarang <sarangaakash@gmail.com>
2018-08-19 13:20:37 -0700

    Write and pass test for saving new user

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -1,7 +1,15 @@
 const assert = require('assert');
+const User = require('../src/user');
 
 describe('Creating records', () => {
-    it('Saves new users', () => {
-        assert(1 + 1 === 2);
+    it('Saves new users', (done) => {
+        const joe = new User({ name: 'Joe' });
+        assert(joe.isNew);
+        joe.save()
+            .then(() => {
+                //Did it save?
+                assert(!joe.isNew);
+                done();
+            });
     });
 });
--- users/test/test_helper.js
+++ users/test/test_helper.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
 mongoose.connect('mongodb://localhost/users_test');
 
 mongoose.connection
     .once('open', () => console.log('Good to go!'))
     .on('error', (error) => {
         console.warn('warning', error);
+    });
+
+    beforeEach((done) => {
+        mongoose.connection.collections.users.drop(() => {
+            //Ready for next test!
+            done();
+        });
     });
