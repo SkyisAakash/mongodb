40c011e42943d8f974191902d05656342d62b0b5
Aakash Sarang <sarangaakash@gmail.com>
2018-08-19 17:57:50 -0700

    Write and pass test for empty username validation

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -4,7 +4,7 @@ const assert = require('assert');
 describe('Updating records', () => {
     let joe;
     beforeEach((done) => {
-        joe = new User({name: 'Joe'});
+        joe = new User({name: 'Joe', postCount: 0});
         joe.save()
             .then(() => done());
     });
@@ -47,6 +47,15 @@ describe('Updating records', () => {
             User.findByIdAndUpdate(joe._id, {name: 'Alex'}),
             done
         );
+    });
+
+    it('can increment postcount by 1', (done) => {
+        User.update({name:'Joe'}, {$inc: {postCount: 10}})
+            .then(() => User.findOne({name: 'Joe'}))
+            .then((user) => {
+                assert(user.postCount === 10);
+                done();
+            });
     });
 
 });
---
+++ users/test/validation_test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Validating records', () => {
+    it('requires a username', () => {
+        const user = new User({name: undefined});
+        const validationResult = user.validateSync();
+        // user.validate((validationResult)=> {
+
+        // });
+        const { message } = validationResult.errors.name;
+        assert(message === 'Name is required.')
+    });
+});
